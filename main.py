5a05e8472ce9ac2087bbeeb25e812b93dcfc9a21
soroush sahraei <soroush.sahraei@gmail.com>
2023-01-18 11:46:48 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from pyrogram import Client
+status = True
+
+api_id = int(input("API id:"))
+api_hash = input("API hash:")
+
+bot = Client(name="new-client-bot",api_id=api_id,api_hash=api_hash)
+
+def good(s):
+  s = s.split()
+  nice = ["بالا", "دارم"]
+  bad = ["تعویض", "عوض", "معاوضه", "پایین"]
+  for i in nice:
+    if(not i in s):
+      return 0
+  for i in bad:
+    if(i in s):
+      return 0
+  return 1
+
+@bot.on_message()
+async def test_bot(client, message):
+  global status
+  #if message.text in ('hello'):
+  s = message.text
+  if status and good(s):
+    #print(message)
+    await message.reply('استفاده')
+    await bot.send_message(message.from_user.id, 'سلام\nوقتتون بخیر\nاستفاده')
+    status = False
+  if message.text == '!active':
+    status = True
+    await message.reply('Bot activated ✅')
+  if message.text == '!deactive':
+    status = True
+    await message.reply('Bot dactivated ❌')
+  if message.text == '!status':
+    if status:
+      await message.reply('Bot is active ✅')
+    else:
+      await message.reply("Bot isn't active ❌")
+
+bot.run() 
